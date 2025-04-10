d4d69be04662151990cdbc67eafb56fa79c32e3b
Oly-i <al219630@alumnos.uacj.mx>
2025-04-09 20:26:10 -0600

    config.php

--- login/config.php
+++ login/config.php
@@ -1,11 +1,12 @@
 <?php
-define ('USER', 'root');
-define ('PASSWORD', '');
-define ('HOST', 'localhost');
-define ('DATABASE', 'test');
+define('USER', '4585646_hospital'); 
+define('PASSWORD', '}Pu#uZ;k4UD5L)r}');  
+define('HOST', 'fdb1030.awardspace.net');  
+define('DATABASE', '4585646_hospital');  
 
-try{
-    $connection = new PDO ("mysql:host=" .HOST.";dbname=".DATABASE,USER,PASSWORD);
-}catch(PDOException $e){
-    exit("Error: ". $e->getMessage());
+try {
+    $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    exit("Error de conexión: " . $e->getMessage());
 }
